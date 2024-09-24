fa1e6e0c43f9ebdd271df2a0cb767ac6e27bdb08
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-09-24 20:56:44 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,17 +22,36 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [qty, setQty] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, qty, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQty("");
+  }
+
   return (
-    <form className='add-form'>
+    <form onClick={handleSubmit} className='add-form'>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={qty} onChange={(e) => setQty(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item..'></input>
+      <input
+        type='text'
+        placeholder='Item..'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
       <button>Add</button>
     </form>
   );
